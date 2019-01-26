19f84d493f381b885d3360d4b6a2d52f040b1457
rbrandon87 <rbrandon87@gmail.com>
2019-01-26 16:18:52 -0500

    Finished left/right movement and bullet firing.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,5 @@
 import pygame
-
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -14,12 +14,17 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     # Make a ship.
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
+    # Make a group to store bullets in.
+    bullets = Group()
 
     # Start the main loop for the game
     while True:
-        gf.check_events()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        bullets.update()
+
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
